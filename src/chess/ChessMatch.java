8be3736d1e42bbd32e477c65195cf66c5c322c73
Gustavo Ferreira <gu-ferreira@usp.br>
2019-07-07 02:49:58 -0300

    Handling captured pieces

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,5 +1,8 @@
 package chess;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import boardgame.Board;
 import boardgame.Piece;
 import boardgame.Position;
@@ -11,6 +14,9 @@ public class ChessMatch {
 	private int turn;
 	private Color currentPlayer;
 	private Board board;
+	
+	private List<Piece> piecesOnTheBoard = new ArrayList<>();
+	private List<Piece> capturedPieces = new ArrayList<>();
 
 	public ChessMatch() {
 		board = new Board(8, 8);
@@ -63,11 +69,22 @@ public class ChessMatch {
 	
 	private Piece makeMove(Position source, Position target){
 		//Logica para realizar o movimento
+		// primeiro tiro ela da posição de origem
 		Piece p = board.removePiece(source);
+		//tiro uma possivel peça da posição de destino
 		Piece capturedPiece = board.removePiece(target);
+		//coloco a peça de origem no seu destino
 		board.placePiece(p, target);
-		return capturedPiece;
 		
+		// vejo se capturou alguma
+		if (capturedPiece != null){
+			//se capturou eu atualizo minhas listas de controle
+			piecesOnTheBoard.remove(capturedPiece);
+			capturedPieces.add(capturedPiece);
+		}
+		
+		
+		return capturedPiece;		
 	}
 	
 	private void validateSourcePosition(Position position){
@@ -100,6 +117,7 @@ public class ChessMatch {
 	
 	private void placeNewPiece(char column, int row, ChessPiece piece){
 		board.placePiece(piece, new ChessPosition(column, row).toPosition());
+		piecesOnTheBoard.add(piece);
 	}
 	
 	//responsável por iniciar a partida colocando as peças de xadrez no tabuleiro
